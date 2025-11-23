fc38e19245a831ca9937889fcf81904c2781fc56
Topher <cvillarreal1254@gmail.com>
2025-11-23 01:05:31 -0800

    Modified test_cities.py to run 3 tests

--- module-7/test_cities.py
+++ module-7/test_cities.py
@@ -3,8 +3,17 @@ from city_functions import city_country
 
 class TestCityCountry(unittest.TestCase):
     def test_city_country(self):
-        test_input = city_country("santiago", "chile", 5000000)
-        self.assertEqual(test_input, "Santiago, Chile - Population 5000000")
+        # Test input with city and country only
+        first_test = city_country("santiago", "chile")
+        self.assertEqual(first_test, "Santiago, Chile")
+
+        # Test input with city, country, and population
+        second_test = city_country("santiago", "chile", 5000000)
+        self.assertEqual(second_test, "Santiago, Chile - Population 5000000")
+
+        # Test input with city, country, population, and language
+        third_test = city_country("santiago", "chile", 5000000, "spanish")
+        self.assertEqual(third_test, "Santiago, Chile - Population 5000000, Spanish")
 
 if __name__ == '__main__':
     unittest.main()
